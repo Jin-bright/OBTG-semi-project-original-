383a8b96df59159bfc8f3541a44ea5f0833e3857
jungeun819 <xhxhxfh@gmail.com>
2023-01-20 02:39:15 +0900

    정은_column_1번째_커밋

---
+++ src/main/java/com/sh/obtg/column/controller/ColumnListServlet.java
@@ -0,0 +1,24 @@
+package com.sh.obtg.column.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ColumnListServlet
+ */
+@WebServlet("/column/columnList")
+public class ColumnListServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/views/column/columnList.jsp").forward(request, response);
+	}
+
+}
---
+++ src/main/java/com/sh/obtg/column/model/dto/Column.java
@@ -0,0 +1,90 @@
+package com.sh.obtg.column.model.dto;
+
+import java.sql.Date;
+
+public class Column {
+
+	// Filed
+	private int no;
+	private String writer;
+	private String title;
+	private String content;
+	private String originalFilename;
+	private String renamedFilename;
+	private int readCount;
+	private Date regDate;
+	
+	// constructor
+	public Column() {}
+	public Column(int no, String writer, String title, String content, String originalFilename, String renamedFilename,
+			int readCount, Date regDate) {
+		super();
+		this.no = no;
+		this.writer = writer;
+		this.title = title;
+		this.content = content;
+		this.originalFilename = originalFilename;
+		this.renamedFilename = renamedFilename;
+		this.readCount = readCount;
+		this.regDate = regDate;
+	}
+
+	// getter setter
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getOriginalFilename() {
+		return originalFilename;
+	}
+	public void setOriginalFilename(String originalFilename) {
+		this.originalFilename = originalFilename;
+	}
+	public String getRenamedFilename() {
+		return renamedFilename;
+	}
+	public void setRenamedFilename(String renamedFilename) {
+		this.renamedFilename = renamedFilename;
+	}
+	public int getReadCount() {
+		return readCount;
+	}
+	public void setReadCount(int readCount) {
+		this.readCount = readCount;
+	}
+	public Date getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+	
+	// toString
+	@Override
+	public String toString() {
+		return "Column [no=" + no + ", writer=" + writer + ", title=" + title + ", content=" + content
+				+ ", originalFilename=" + originalFilename + ", renamedFilename=" + renamedFilename + ", readCount="
+				+ readCount + ", regDate=" + regDate + "]";
+	}
+	
+}
