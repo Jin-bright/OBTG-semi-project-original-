60fab3870fa4264f35ad49b9f741c0a474a39046
borussia444423 <jung78978978@naver.com>
2023-01-27 10:20:00 +0900

    인철_커

---
+++ src/main/java/com/sh/obtg/admin/controller/AdminMemberFinderServlet.java
@@ -0,0 +1,51 @@
+package com.sh.obtg.admin.controller;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sh.obtg.member.model.dto.Member;
+import com.sh.obtg.member.model.service.MemberService;
+
+
+@WebServlet("/admin/memberFinder")
+public class AdminMemberFinderServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private MemberService memberService = new MemberService();
+	
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 1. 사용자입력값 가져오기 - searchType, searchKeyword
+		String searchType = request.getParameter("searchType");
+		String searchKeyword = request.getParameter("searchKeyword");
+		
+		Map<String, String> param = new HashMap<>();
+		param.put("searchType", searchType);
+		param.put("searchKeyword", searchKeyword);
+		System.out.println("param = " + param);
+		
+		// 2. 업무로직 - 회원검색 
+		List<Member> members = memberService.searchMember(param);
+		System.out.println("members = " + members);
+		
+		// 3. forward(view단처리)
+		request.setAttribute("members", members);
+		request.getRequestDispatcher("/WEB-INF/views/admin/memberList.jsp")
+			.forward(request, response);
+		
+		
+	}
+
+}
+
+
+
+
+
--- src/main/java/com/sh/obtg/member/model/service/MemberService.java
+++ src/main/java/com/sh/obtg/member/model/service/MemberService.java
@@ -1,8 +1,13 @@
 package com.sh.obtg.member.model.service;
 
-import static com.sh.obtg.common.JdbcTemplate.*;
+import static com.sh.obtg.common.JdbcTemplate.close;
+import static com.sh.obtg.common.JdbcTemplate.commit;
+import static com.sh.obtg.common.JdbcTemplate.getConnection;
+import static com.sh.obtg.common.JdbcTemplate.rollback;
 
 import java.sql.Connection;
+import java.util.List;
+import java.util.Map;
 
 import com.sh.obtg.member.model.dao.MemberDao;
 import com.sh.obtg.member.model.dto.Member;
@@ -10,12 +15,48 @@ import com.sh.obtg.member.model.dto.Member;
 public class MemberService {
 	
 	private MemberDao memberDao = new MemberDao();
+	
 	public Member selectOneMember(String memberId) {
 		Connection conn = getConnection();
 		System.out.println(conn);
 		Member member = memberDao.selectOneMember(conn, memberId);
 		close(conn);
 		return member;
+	}
+	public int updateMemberRole(String memberId, String memberRole) {
+		Connection conn = getConnection();
+		int result = 0;
+		try {
+			result = memberDao.updateMemberRole(conn, memberId, memberRole);
+			commit(conn);
+		} catch (Exception e) {
+			rollback(conn);
+			throw e;
+		} finally {
+			close(conn);
+		}
+		return result;
+	}
+	
+	public List<Member> searchMember(Map<String, String> param) {
+		Connection conn = getConnection();
+		List<Member> members = memberDao.searchMember(conn, param);
+		close(conn);
+		return members;
+	}
+	
+	public List<Member> selectAllMember(Map<String, Object> param) {
+		Connection conn = getConnection();
+		List<Member> members = memberDao.selectAllMember(conn, param);
+		close(conn);
+		return members;
+	}
+	
+	public int selectTotalCount() {
+		Connection conn = getConnection();
+		int totalCount = memberDao.selectTotalCount(conn);
+		close(conn);
+		return totalCount;
 	}
 
 }
