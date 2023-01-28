c8e62744c949c0338af782142cb183235a8e06b1
Lehanaa <eowk3508@gmail.com>
2023-01-28 21:25:55 +0900

    회원가입 알림창이 안떠요 ㅜㅜ

--- src/main/java/com/sh/obtg/member/controller/MemberEnrollServlet.java
+++ src/main/java/com/sh/obtg/member/controller/MemberEnrollServlet.java
@@ -1,6 +1,7 @@
 package com.sh.obtg.member.controller;
 
 import java.io.IOException;
+import java.sql.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,12 +10,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.sh.obtg.member.model.dto.Gender;
+import com.sh.obtg.member.model.dto.Member;
+import com.sh.obtg.common.HelloMvcUtils;
+import com.sh.obtg.member.model.service.MemberService;
+
 /**
  * Servlet implementation class MemberEnrollServlet
  */
 @WebServlet("/member/memberEnroll")
 public class MemberEnrollServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private MemberService memberService = new MemberService();
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
@@ -34,24 +41,41 @@ public class MemberEnrollServlet extends HttpServlet {
 		  try { 
 			  
 		  String memberId = request.getParameter("memberId"); 
-		  String style = request.getParameter("style"); 
+		  String[] _style = request.getParameterValues("style"); 
 		  String name = request.getParameter("name");
-		  String password = request.getParameter("password");
-		  String email = request.getParameter("password");
-		  String phone = request.getParameter("password");
-		  String _birthday = request.getParameter("password");
-		  String nickname = request.getParameter("password");
-		  String _gender = request.getParameter("password");
-		  /*String password = request.getParameter("password");
-		  String password = request.getParameter("password");*/
+		  String password = HelloMvcUtils.getEncryptedPassword(request.getParameter("password"), memberId);
+		  String email = request.getParameter("email");
+		  String phone = request.getParameter("phone");
+		  String _birthday = request.getParameter("birthday");
+		  String nickname = request.getParameter("nickname");
+		  String _gender = request.getParameter("gender");
+		  String introduce = request.getParameter("introduce");
+		  String original = request.getParameter("original");
+		  String renamed = request.getParameter("renamed");
 				  
-		  }catch(Exception e) {
-				// 회원가입실패 메세지
-				session.setAttribute("msg", "회원가입 실패했습니다.");
-
-				// 예외로깅
-				e.printStackTrace();
+		// 후처리
+		Date birthday = !"".equals(_birthday) ? Date.valueOf(_birthday) : null;
+		Gender gender = _gender != null ? Gender.valueOf(_gender) : null;
+		String style = _style != null ? String.join(",", _style) : null;
+					
+		Member member = new Member(memberId, style, name, password, email, phone, birthday, null, null, nickname, gender, introduce, original, renamed);
+		System.out.println(member);
+					
+					
+		// 2. 업무로직 - db insert
+		int result = memberService.insertMember(member);
+		
+			if(result > 0) {
+				// 회원가입성공 메세지
+				session.setAttribute("msg", "회원가입을 축하드립니다.");
 			}
+		}catch(Exception e) {
+			// 회원가입실패 메세지
+			session.setAttribute("msg", "회원가입 실패했습니다.");
+
+			// 예외로깅
+			e.printStackTrace();
+		}
 			
 			// 4. 리다이렉트 - /mvc/
 			response.sendRedirect(request.getContextPath() + "/");
